6f3a20a07af96a677c85bb3154f920bd833c000d
Ngoc Tien <ngoctienvc@gmail.com>
2014-03-01 12:59:12 +0700

    UpLoad

---
+++ Baitap4/Baitap4/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Baitap4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.listBoxEvents = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(46, 8);
+            this.textBoxX.Multiline = true;
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(42, 24);
+            this.textBoxX.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Y";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(158, 9);
+            this.textBoxY.Multiline = true;
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(48, 23);
+            this.textBoxY.TabIndex = 3;
+            // 
+            // listBoxEvents
+            // 
+            this.listBoxEvents.FormattingEnabled = true;
+            this.listBoxEvents.Location = new System.Drawing.Point(9, 56);
+            this.listBoxEvents.Name = "listBoxEvents";
+            this.listBoxEvents.Size = new System.Drawing.Size(289, 368);
+            this.listBoxEvents.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 468);
+            this.Controls.Add(this.listBoxEvents);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Su kien chuot";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.ListBox listBoxEvents;
+    }
+}
+
---
+++ Baitap4/Baitap4/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Baitap4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBoxX.Text = e.X.ToString();
+            textBoxY.Text = e.Y.ToString();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string suKien = "";
+            // Neu bam nut trai chuot
+            if (e.Button == MouseButtons.Left)
+            {
+                suKien = "Left mouse click at " + e.X.ToString() + "," +
+                e.Y.ToString();
+            }
+            // Neu bam nut phai chuot
+            else if (e.Button == MouseButtons.Right)
+            {
+                suKien = "Right mouse click at " + e.X.ToString() + "," +
+                e.Y.ToString();
+            }
+            listBoxEvents.Items.Add(suKien);
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            string suKien = "Mouse enter";
+            listBoxEvents.Items.Add(suKien);
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            string suKien1 = "Mouse Leave";
+            listBoxEvents.Items.Add(suKien1);
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+            string suKien2 = "Mouse Hover";
+            listBoxEvents.Items.Add(suKien2);
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string suKien = "";
+            // Neu bam nut trai chuot
+            if (e.Button == MouseButtons.Left && e.Button == MouseButtons.Left)
+            {
+                suKien = "Left mouse double click at " + e.X.ToString() + "," +
+                e.Y.ToString();
+            }
+            // Neu bam nut phai chuot
+            else if (e.Button == MouseButtons.Right && e.Button == MouseButtons.Right)
+            {
+                suKien = "Right mouse double click at " + e.X.ToString() + "," +
+                e.Y.ToString();
+            }
+            listBoxEvents.Items.Add(suKien);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            string suKien = "";
+            // Neu bam nut trai chuot
+            if (e.Button == MouseButtons.Left)
+            {
+                suKien = "Left mouse up at " + e.X.ToString() + "," +
+                e.Y.ToString();
+            }
+            // Neu bam nut phai chuot
+            else if (e.Button == MouseButtons.Right)
+            {
+                suKien = "Right mouse up at " + e.X.ToString() + "," +
+                e.Y.ToString();
+            }
+            listBoxEvents.Items.Add(suKien);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            string suKien = "";
+            // Neu bam nut trai chuot
+            if (e.Button == MouseButtons.Left)
+            {
+                suKien = "Left mouse down at " + e.X.ToString() + "," +
+                e.Y.ToString();
+            }
+            // Neu bam nut phai chuot
+            else if (e.Button == MouseButtons.Right)
+            {
+                suKien = "Right mouse down at " + e.X.ToString() + "," +
+                e.Y.ToString();
+            }
+            listBoxEvents.Items.Add(suKien);
+        }
+
+
+        
+
+       
+      
+    }
+}
